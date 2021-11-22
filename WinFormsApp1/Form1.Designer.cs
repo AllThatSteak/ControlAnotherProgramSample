7f8c813600205e042c6a9edeb02ea3b892aa4b46
sdota <sdota@DESKTOP-IFQ20OI>
2021-11-21 16:43:15 -0800

    Add project files.

---
+++ WinFormsApp1/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mainDisplay = new System.Windows.Forms.PictureBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnDoStuff = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.mainDisplay)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // mainDisplay
+            // 
+            this.mainDisplay.Location = new System.Drawing.Point(96, 12);
+            this.mainDisplay.Name = "mainDisplay";
+            this.mainDisplay.Size = new System.Drawing.Size(692, 340);
+            this.mainDisplay.TabIndex = 0;
+            this.mainDisplay.TabStop = false;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(12, 12);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnDoStuff
+            // 
+            this.btnDoStuff.Location = new System.Drawing.Point(12, 41);
+            this.btnDoStuff.Name = "btnDoStuff";
+            this.btnDoStuff.Size = new System.Drawing.Size(75, 23);
+            this.btnDoStuff.TabIndex = 2;
+            this.btnDoStuff.Text = "Do Stuff";
+            this.btnDoStuff.UseVisualStyleBackColor = true;
+            this.btnDoStuff.Click += new System.EventHandler(this.btnDoStuff_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnDoStuff);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.mainDisplay);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.mainDisplay)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private PictureBox mainDisplay;
+        private Button btnStart;
+        private Button btnDoStuff;
+    }
+}
---
+++ WinFormsApp1/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern void SetParent(IntPtr hwndChild, IntPtr hwndNewParent);
+
+        [DllImport("User32.dll")]
+
+        static extern int SetForegroundWindow(IntPtr point);
+
+
+        public Process? Process = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (Process == null)
+            {
+                Process = new Process();
+                Process.StartInfo.FileName = "notepad.exe";
+                Process.Start();
+            }
+        }
+
+        private void btnDoStuff_Click(object sender, EventArgs e)
+        {
+            if (Process != null)
+            {
+                SetForegroundWindow(Process.MainWindowHandle);
+                SendKeys.Send("💩 💩 💩 💩 💩 💩 💩 💩 💩 💩 💩 💩 💩 💩 💩 💩");
+                SendKeys.Send("{ENTER}");
+            }
+        }
+    }
+}
